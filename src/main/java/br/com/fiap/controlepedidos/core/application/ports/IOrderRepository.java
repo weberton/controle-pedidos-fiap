9b344f01b965a6a0fb695519dbdb01387cf5954a
Luan Vieira <luandasilvavieira@hotmail.com>
2025-05-25 16:55:12 -0300

    implementando repositorio de order para processo de checkout e realizando os ajustes necessarios no ports de pagamento

--- src/main/java/br/com/fiap/controlepedidos/core/application/ports/IOrderRepository.java
+++ src/main/java/br/com/fiap/controlepedidos/core/application/ports/IOrderRepository.java
@@ -1,4 +1,15 @@
 package br.com.fiap.controlepedidos.core.application.ports;
 
-public interface IOrderRepository {
+import br.com.fiap.controlepedidos.core.domain.entities.Order;
+import br.com.fiap.controlepedidos.core.domain.enums.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IOrderRepository extends JpaRepository<Order, UUID> {
+
+    Page<Order> findByOrderStatusOrderByUpdatedAtAsc(OrderStatus status, Pageable pageable);
+
 }
