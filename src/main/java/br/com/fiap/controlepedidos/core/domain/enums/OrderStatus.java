7658c6a7f6cc643b15cf13b21f12411cd52244de
Luan Vieira <luandasilvavieira@hotmail.com>
2025-05-25 17:09:05 -0300

    Cria entidades e enumerator para order e payment ambos utilizados no processo de checkout

--- src/main/java/br/com/fiap/controlepedidos/core/domain/enums/OrderStatus.java
+++ src/main/java/br/com/fiap/controlepedidos/core/domain/enums/OrderStatus.java
@@ -1,4 +1,19 @@
 package br.com.fiap.controlepedidos.core.domain.enums;
 
 public enum OrderStatus {
+    CREATED("Criado"),
+    RECEIVED("Recebido"),
+    INPREP("Preparação"),
+    READY("Pronto"),
+    DONE("Finalizado");
+
+    private final String desc;
+
+    OrderStatus(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDescription() {
+        return desc;
+    }
 }
--- src/main/java/br/com/fiap/controlepedidos/core/domain/enums/PaymentStatus.java
+++ src/main/java/br/com/fiap/controlepedidos/core/domain/enums/PaymentStatus.java
@@ -1,4 +1,17 @@
 package br.com.fiap.controlepedidos.core.domain.enums;
 
 public enum PaymentStatus {
+    WAITING("Aguardando"),
+    PAID("Pago"),
+    CANCELLED("Cancelado");
+
+    private final String desc;
+
+    PaymentStatus(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDescription() {
+        return desc;
+    }
 }
