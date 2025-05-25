7728defa689fc00abadc68ffce1f77a6588a9472
Luan Vieira <luandasilvavieira@hotmail.com>
2025-05-25 17:09:53 -0300

    cria tabela order no banco- flyway

--- src/main/resources/db/migration/V6__Create_Table_Order.sql
+++ src/main/resources/db/migration/V6__Create_Table_Order.sql
@@ -1,31 +1,23 @@
-CREATE TABLE carts
+CREATE TABLE orders
 (
-    cart_id     BINARY(16)       NOT NULL,
-    customer_id BINARY(16)       NULL,
-    total_cents BIGINT    NOT NULL,
-    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (cart_id),
-    CONSTRAINT fk_carts_customer
+    order_id     BINARY(16)       NOT NULL,
+    order_number INT NOT NULL,
+    customer_id  BINARY(16)       NULL,
+    cart_id      BINARY(16)       NULL,
+    order_status VARCHAR(40) NULL,
+    total_cents  float     NOT NULL,
+    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (order_id),
+    CONSTRAINT fk_orders_customer
         FOREIGN KEY (customer_id)
             REFERENCES customers (id)
+            ON DELETE SET NULL,
+    CONSTRAINT fk_orders_cart
+        FOREIGN KEY (cart_id)
+            REFERENCES carts (cart_id)
             ON DELETE SET NULL
 );
 
-CREATE TABLE cart_items
-(
-    cart_item_id   BINARY(16)       NOT NULL PRIMARY KEY,
-    cart_id        BINARY(16)       NOT NULL,
-    product_id     BINARY(16)       NOT NULL,
-    quantity       INT    NOT NULL CHECK (quantity >= 1),
-    price_cents    BIGINT NOT NULL CHECK (price_cents >= 0),
-    subtotal_cents BIGINT NOT NULL CHECK (subtotal_cents >= 0),
-    CONSTRAINT fk_cartitems_cart FOREIGN KEY (cart_id)
-        REFERENCES carts (cart_id)
-        ON DELETE CASCADE,
-    CONSTRAINT fk_cartitems_product FOREIGN KEY (product_id)
-        REFERENCES products (id)
-);
-
-CREATE INDEX idx_cart_items_cart_id    ON cart_items(cart_id);
-CREATE INDEX idx_cart_items_product_id ON cart_items(product_id);
+CREATE INDEX idx_orders_order_id ON orders (order_id);
+CREATE INDEX idx_orders_cart_id ON orders (cart_id);
