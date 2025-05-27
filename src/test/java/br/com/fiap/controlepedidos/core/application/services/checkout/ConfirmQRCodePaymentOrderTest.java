574791900c50a94c7d07dfe49bf0faa10f789cd7
Luan Vieira <luandasilvavieira@hotmail.com>
2025-05-27 00:59:16 -0300

    Criando alguns testes para processo de checkout e orders

---
+++ src/test/java/br/com/fiap/controlepedidos/core/application/services/checkout/ConfirmQRCodePaymentOrderTest.java
@@ -0,0 +1,42 @@
+package br.com.fiap.controlepedidos.core.application.services.checkout.impl;
+
+import br.com.fiap.controlepedidos.core.application.services.checkout.ConfirmQRCodePaymentOrder;
+import br.com.fiap.controlepedidos.core.application.services.order.FindOrderByIdService;
+import br.com.fiap.controlepedidos.core.application.services.order.PayOrderService;
+import br.com.fiap.controlepedidos.core.domain.entities.Order;
+import br.com.fiap.controlepedidos.core.domain.entities.Payment;
+import br.com.fiap.controlepedidos.core.domain.enums.OrderStatus;
+import br.com.fiap.controlepedidos.core.domain.enums.PaymentStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ConfirmQRCodePaymentOrderImpl implements ConfirmQRCodePaymentOrder {
+
+    private final FindOrderByIdService findOrderByIdService;
+    private final PayOrderService payOrderService;
+
+    public ConfirmQRCodePaymentOrderImpl(FindOrderByIdService findOrderByIdService, PayOrderService payOrderService) {
+        this.findOrderByIdService = findOrderByIdService;
+        this.payOrderService = payOrderService;
+    }
+
+    @Override
+    public Payment confirmQrCodePayment(UUID orderId, float paidValue) throws Exception {
+        try {
+            Order orderToPay = findOrderByIdService.getById(orderId);
+
+            Payment result = payOrderService.payOrder(orderToPay, paidValue);
+
+            if (result.getPaymentStatus().equals(PaymentStatus.PAID)) {
+                return result;
+            } else {
+                //TODO ao implementar pagamento é necessário atualizar o pagamento aqui também.
+                return new Payment();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
