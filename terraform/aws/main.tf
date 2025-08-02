93d2c885b8b139e59a18dada1c29198b729bf292
Weberton <faria.weberton@gmail.com>
2025-08-02 11:05:55 -0400

    Add terraform to create the infra on aws

---
+++ terraform/aws/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = "controlepedidos-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  map_public_ip_on_launch = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.33"
+  subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = false
+
+  access_entries = {
+    your_user = {
+      principal_arn = "arn:aws:iam::967246349191:root"
+      policy_associations = {
+        admin = {
+          policy_arn   = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = { type = "cluster" }
+        }
+      }
+    }
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = [var.node_instance_type]
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 1
+    }
+  }
+}
