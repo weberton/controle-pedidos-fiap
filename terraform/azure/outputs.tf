b27192ac5471a0fcd6e5b1bfbb43fd9d580fee51
Luan Vieira <luandasilvavieira@hotmail.com>
2025-08-03 22:09:41 -0300

    Creating azure infrastructure using terraform

---
+++ terraform/azure/outputs.tf
@@ -0,0 +1,20 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
